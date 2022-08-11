75bc95c11548c4d77abc76bb0b54ddb1a21cc2c8
Geng-Wung <gengxis92@gmail.com>
2022-08-11 13:02:04 -0400

    commit tree a379054484c442ae31037b4eb18987728efecfd0

---
+++ index.js
@@ -0,0 +1,48 @@
+function add() {
+  }
+
+  function subtract() {
+}
+
+function multiply() {
+}
+
+function divide() {
+}
+
+function add (a,b) {
+  return a+b;
+}
+
+function subtract(a,b) {
+  return a-b;
+}
+
+function multiply(a,b) {
+  return a*b;
+}
+
+function divide(a,b) {
+  return a / b;
+}
+
+function increment(n) {
+  n++;
+  return n;
+}
+
+function decrement(n) {
+  n--;
+  return n;
+}
+
+//test failed, asked for base of 10 radix is set to 10 but will not pass
+function makeInt(n) {
+  parseInt(n, 10);
+  return n;
+}
+
+function preserveDecimal(n) {
+  parseFloat(n);
+  return n;
+}
